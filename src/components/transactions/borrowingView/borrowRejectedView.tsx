83d6f830415b49e9ded26396544346c0bc333594
Pongot, Jophiel <jophiel.telen@gmail.com>
2023-11-18 14:11:34 +0800

    Transaction: Fixed na tanan (some changes needs action specially buttons)

--- src/components/transactions/borrowingView/borrowRejectedView.tsx
+++ src/components/transactions/borrowingView/borrowRejectedView.tsx
@@ -49,27 +49,40 @@ function BorrowRejectedView() {
      {
         idNumber: 201854265,
         name: 'Sofia Dara Alilin',
-        Dept: 'BSCHEMISTRY'
+        Dept: 'BSCHEMISTRY',
+        Section: 'CHEM3H2'
+      }   
+     ]
+     const membersDetails =[
+         {
+            idNumber: 2018542654,
+            Name: 'Joel Morongot',
         
-     }   
-    ]
-    const membersDetails =[
-        {
-           idNumber: 2018542654,
+         },
+         {
+           idNumber: 2020100768,
            Name: 'Joel Morongot',
        
-        },
+        },   
         {
-          idNumber: 2020100768,
-          Name: 'Joel Morongot',
-      
-       },   
+         idNumber: 2020100768,
+         Name: 'Trissa Saman Asali Mulan Yee',
+     
+      }   
+     ]
+ 
+     const instructor = [
        {
-        idNumber: 2020100768,
-        Name: 'Trissa Saman Asali Mulan Yee',
-    
-     }   
-    ]
+         idNumber: 2055060541,
+         Name: 'Ms. Jerilyn Yare',
+       }
+     ]
+ 
+     const labtech = [
+       {
+         Name: "Prince Kurt Laurence",
+       }
+     ]
 
     let totalQuantity = 0;
     pendingItems.forEach((item) => {
@@ -79,136 +92,171 @@ function BorrowRejectedView() {
     });
    
 
-
-  return (
-    <section className="content">
-      <div className="titleHeader">
-      <div className='arrowTitleView'>
-          <Link to ='/borrowing/pending'>
-             <GoArrowLeft className='iconLeftArrow'/>
-          </Link>
-          <div className='titleName'> 
-          Viewing Transaction 
+    return (
+      <section className="content">
+        {/* Title ni siya */}
+        <div className="titleHeader">
+          <div className="arrowTitleView">
+            <Link to ='/borrowing/pending'>
+              <GoArrowLeft className='iconLeftArrow'/>
+            </Link>
+            <div className="titleName">
+              Viewing Transaction
+            </div>
           </div>
         </div>
-      </div>
-
-
+  
+        {/* Body */}
         {pendingItems.map((item) => (
-          <div key={item.id}>
-              
-          <div className='transaction-header'>
-              <div className='transaction-header-status-rejected'>
-              Status: <span>{item.status}</span>
+          <div className="transbody">
+            <div className="receiptContainer">
+  
+              {/* Status Head */}
+              <div className='transaction-header'>
+                <div className='transaction-header-status-rejected'>
+                  Status: <span>{item.status}</span>
+                </div>
+  
               </div>
-
-              
-          </div>
-
-
-          
-          <div className='transactionViewContainer'>
-              {/* inside content sa white container */}
-
-
-              <div className='transactionView-title'>
-                Transaction ID #<span className='bold-item-id'>{item.id}</span>
-              </div>
-
-              {/* --student items */}
-              {studentDetails.map((student) => (                   
-                  <div className='viewStudentInfo'>
-                    <div className='iconProfileContainer'> 
+  
+              {/* White Container */}
+              <div className="transactionViewContainer">
+  
+                {/* Trans ID */}
+                <div className="transactionView-title">
+                  Transaction ID #<span className='bold-item-id'>{item.id}</span>
+                </div>
+  
+                {/* Student Details */}
+                {studentDetails.map((student) => (
+                  <div className="firstRow">
+                    <div className='viewStudentInfo'>
+                      <div className='iconProfileContainer'> 
                         <CgProfile className='iconProfile'/>
-                    </div>
-                    <div className='student-details'>
-                      <div className='student-Name'>
-                        {student.name}
                       </div>
-                      <div className='student-ID-Dept'>
-                        {student.idNumber}-{student.Dept}
+                      <div className='student-details'>
+                        <div className='student-Name'>
+                          {student.name}
+                        </div>
+                        <div className='student-ID-Dept'>
+                          {student.idNumber}-{student.Dept}
+                        </div>
                       </div>
                     </div>
+                      <div className="sectionContainer">
+                        Section: <b>{student.Section}</b>
+                      </div>
                   </div>
-                  ))}   
-
-              {/* --members view       */}
-              <div className='viewTitleLabel'>Members </div>
-                      
-                  <div className='viewMembersInfo'>
-                  {membersDetails.map((member, index)=>(       
-                      <div className='viewEachInfo'>
+                ))}
+  
+                {/* Second Row*/}
+                <div className="memInsCont">
+  
+                  {/* Members */}
+                  <div className="members">
+                    <div className="viewTitleLabel">Members</div>
+                    <div className='viewMembersInfo'>
+                      {membersDetails.map((member, index)=>(       
+                        <div className='viewEachInfo'>
                           <div className='eachIndex'>
                             {index + 1}
                           </div>
                           <div className='eachmemID'>
-                      
-                          {member.idNumber}
+                            {member.idNumber}
                           </div>
                           <div className='eachmemNAME'>
-                              {member.Name}
-                              
+                            {member.Name}       
                           </div>
-                      </div>
-              ))}
-              </div>
-
-                {/* --selected view */}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+  
+                  {/* Instructor */}
+                  <div className="instructor">
+                    <div className='viewTitleLabel'>Instructor </div>
+                    <div className='viewMembersInfo'>
+                      {instructor.map((instructor, index)=>(       
+                        <div className='viewEachInfo'>
+                          <div className='eachmemNAME'>
+                            {instructor.Name}       
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+  
+                {/* Third Row = Items Selected */}
                 <div className='viewTitleLabel'> Selected Items </div>
                 <div className='viewItemsSelected'>
-                    {item.items.map((borrowedItems, index) => (
-                        <div className='viewEachInfo' key={index}>
-                            <div className='eachIndex'>
-                                {index + 1}
-                            </div>
-                            <div className='eachItemDetails'>
-                                {borrowedItems.name}
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;x&nbsp;
-                                {borrowedItems.quantity}
-                            </div>
-                        </div>
-                    ))}
+                  {item.items.map((borrowedItems, index) => (
+                    <div className='viewEachInfo' key={index}>
+                      <div className='eachIndex'>
+                        {index + 1}
+                      </div>
+                      <div className='eachItemDetails'>
+                        {borrowedItems.name}
+                      </div>
+                    </div>
+                  ))}
                 </div>
-
-              <div className='totalQuantityLabel'>
-                <span className='totalText'>Quantity Borrowed:</span>
-                &nbsp;&nbsp;
-                {totalQuantity}
-              </div>
-
-              <div className='viewDateTime'>
+  
+                <div className='totalQuantityLabel'>
+                  <span className='totalText'>Quantity Borrowed:</span>
+                  &nbsp;&nbsp;
+                  {totalQuantity}
+                </div>
+  
+                {/* Last Row Approved by View */}
+                <div className="approvedCont">
+                  <div className='viewTitleLabel'>Approved by: </div>
+                  <div className='viewMembersInfo'>
+                    {instructor.map((instructor, index)=>(       
+                      <div className='viewEachInfo'>
+                        <ul className='eachmemNAME'>
+                          <li>Instructor: <b>{instructor.Name}</b></li>       
+                        </ul>
+                      </div>
+                    ))}
+                    {labtech.map((labtech, index)=>(       
+                      <div className='viewEachInfo'>
+                        <ul className='eachmemNAME'>
+                          <li>Lab Technician: <b>{labtech.Name} </b>
+                          </li>  
+                        </ul>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+  
+  
+                {/* Time and Date Created */}
+                <div className='viewDateTime'>
                   <div className='viewDate'>
-                      {item.date}
+                    {item.date}
                   </div>
                   <div className='viewTime'>
-                      {item.time}
+                    {item.time}
                   </div>
-
-              </div>
-
-            
-              <div className='darabyue'>
-                  
-                  <Link to= "/borrowing/pending" className='viewButtonAccept'>
-                      Done View
-                  </Link>
-
-              </div>
-
+                </div>
+              </div> 
   
-
-
-          </div> 
-
-         </div>
-
-            
-            
-            
-
+              {/* buttons */}
+              <div className="darabyue">
+                <Link to= "/borrowing/accepted" className='viewButtonAccept'>
+                  Done View
+                </Link>
+              </div>
+  
+  
+            </div>
+          </div>
         ))}
-    </section>
-  );
+        
+      </section>
+    );
+
 }
 
 export default BorrowRejectedView;
